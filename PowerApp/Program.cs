3f424d5b1d68028294ab5a9838bec3b35df5bcb8
Ch. Kleideris <kleiderisch@gmail.com>
2025-06-30 15:43:53 +0300

    feat: add PowerApp project

---
+++ PowerApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+namespace PowerApp
+{
+    /// <summary>
+    /// Calculates a base number b raised in power (b ^ power), ex: 2^10 = 1024
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myBase = 2;
+            int power = 10;
+            BigInteger result = 1;
+
+            for (int i = 1; i <= power; i++)
+            {
+                result *= myBase;
+            }
+            Console.WriteLine($"{myBase} ^ {power} = {result}");
+        }
+    }
+}
