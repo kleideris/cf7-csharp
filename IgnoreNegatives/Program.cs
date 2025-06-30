9f22295d88a539ca6a300debcf0ba1808f64b514
Ch. Kleideris <kleiderisch@gmail.com>
2025-06-30 14:49:16 +0300

    feat: add IgnoreNegatives project

---
+++ IgnoreNegatives/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace IgnoreNegatives
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            int negativeCount = 0;
+
+            try
+            {
+                do
+                {
+                    Console.WriteLine("Please insert a number (integer)");
+                    num = int.Parse(Console.ReadLine()!);
+                    if (num > 0) continue;
+                    if (num == 0) break;
+                    negativeCount++;
+                } while (true);
+
+                Console.WriteLine("Negative Count: " + negativeCount);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);                
+            }
+        }
+    }
+}
