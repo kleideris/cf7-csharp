bbe5520411ab00d5e21f0e1d8cebecb64bc6c377
Ch. Kleideris <kleiderisch@gmail.com>
2025-07-03 15:46:14 +0300

    fix: ensure proper rendering of euro symbol with UTF-8 encoding and and clean up formatting

--- EuroToDollarApp/Program.cs
+++ EuroToDollarApp/Program.cs
@@ -1,27 +1,37 @@
-﻿using System.Globalization;
+﻿using System.Text;
 
 namespace EuroToDollarApp
 {
+    /// <summary>
+    /// Reads from standard input an int that represents an amount of euros and converts it to dollars.
+    /// Its assumed that the parity rate is 1 Euro = 1.17 USD.
+    /// </summary>
     internal class Program
     {
         static void Main(string[] args)
         {
-            float euro = 0;
-            float dollar = 0;
+            const int CENTS_PER_DOLLAR = 100;
+            decimal dollars;
+            decimal cents;
 
-            Console.WriteLine("Please insert the value of Euros:");
-            euro = int.Parse(Console.ReadLine()!);
+            Console.WriteLine("Please insert the amount in Euros.");
+            if (!decimal.TryParse(Console.ReadLine(), out decimal inputEuros)) {
+                Console.WriteLine("Error in input");
+            }
 
-            dollar = ConvertEuroToDollars(euro);
+            dollars = ConvertEuroToDollars(inputEuros);
+            cents = dollars * CENTS_PER_DOLLAR;
 
-            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
-            Console.WriteLine($"{euro} Euro = {dollar:C}$");
+
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine($"\u20AC {inputEuros:N2} = \u0024 {dollars:N2} or {cents, 5:N2} \u0024 cents");
         }
 
-        public static float ConvertEuroToDollars(float money)
+        public static decimal ConvertEuroToDollars(decimal inputEuros)
         {
-            const float ConversionRate = 1.16F;
-            return money *= ConversionRate;
+            const decimal RATE = 1.17m;   
+            
+            return inputEuros * RATE;
         }     
     }
 }
