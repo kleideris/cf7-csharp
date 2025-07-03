68dc96ae17f79bb90d6c3fbc81461a2273807324
Ch. Kleideris <kleiderisch@gmail.com>
2025-07-03 14:42:04 +0300

    feat: add basic console app to check if a number is prime

---
+++ PrimesApp/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace PrimesApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            int number = int.Parse(Console.ReadLine()!);
+
+            if (IsPrime(number))
+            {
+                Console.WriteLine($"{number} is prime.");
+            } else
+            {
+                Console.WriteLine($"{number} is not prime.");
+            }
+        }
+
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
