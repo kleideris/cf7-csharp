12a85225a43de0068ea85d91c4aad42aa5f8a54f
Ch. Kleideris <kleiderisch@gmail.com>
2025-07-03 14:32:35 +0300

    feat: add euro to dollar conversion with localized currency formatting

---
+++ EuroToDollarApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace EuroToDollarApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float euro = 0;
+            float dollar = 0;
+
+            Console.WriteLine("Please insert the value of Euros:");
+            euro = int.Parse(Console.ReadLine()!);
+
+            dollar = ConvertEuroToDollars(euro);
+
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+            Console.WriteLine($"{euro} Euro = {dollar:C}$");
+        }
+
+        public static float ConvertEuroToDollars(float money)
+        {
+            const float ConversionRate = 1.16F;
+            return money *= ConversionRate;
+        }     
+    }
+}
