49d3591b5a8ea389b834ef1cea4ac66eb1885252
Ch. Kleideris <kleiderisch@gmail.com>
2025-07-17 11:44:00 +0300

    feat: add AbstractClassesApp, showcase abstract and override keyword

---
+++ AbstractClassesApp/Model/Cat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesApp.Model
+{
+    internal class Cat : AbstractAnimal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("Cat says Hello");
+        }
+
+        //public void Eat()
+        //{
+        //    base.Eat();
+        //    //Console.WriteLine("Cat is eating");
+        //}
+
+        public override string ToString()
+        {
+            return "Id: " + Id + ", Name: " + Name + ", Age: " + Age;
+        }
+    }
+}
