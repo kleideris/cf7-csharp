52948f8f284cb2d3f9a2fc43c30a03cb2bf9f00b
Ch. Kleideris <kleiderisch@gmail.com>
2025-07-17 11:40:35 +0300

    feat: add PontMonolithic, showcase basic crud and service API

---
+++ PointMonolithic/Program.cs
@@ -0,0 +1,36 @@
+﻿using PointMonolithic.Model;
+
+namespace PointMonolithic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new() { X = 1 };
+            Point p2 = new() { X = 2 };
+            Point p3 = new() { X = 3 };
+
+            p1.InsertPoint();
+            p2.InsertPoint();
+            p3.InsertPoint();
+
+            Point? deletePoint = p3.DeletePoint();
+            p3.UpdatePoint(new Point { X = 55 });
+
+
+            //Points Traversal:
+            //1st way
+            foreach (Point p in Point.GetAllPoints())
+            {
+                Console.WriteLine(p);
+            }
+
+            //2nd way
+            Point.GetAllPoints().ForEach(p => Console.WriteLine(p));
+
+            //3rd way
+            Point.Points.ForEach(Console.WriteLine);
+
+        }
+    }
+}
