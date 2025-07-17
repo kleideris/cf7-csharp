36eba05cffa7d06a66dddbfc699a6b2a4eb97c82
Ch. Kleideris <kleiderisch@gmail.com>
2025-07-17 19:58:37 +0300

    feat: add SinglyLinkedList, showcase SLLList implementation

---
+++ SinglyLinkedList/Model/SLList.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SinglyLinkedList.Model
+{
+    internal class SLList<T>
+    {
+        public ListNode<T>? Head { get; set; }
+
+        public SLList()
+        {
+            Head = null;
+        }
+
+        public void InsertFirst(T t)
+        {
+            ListNode<T> tmp = new()
+            {
+                Value = t,
+                Next = Head
+            };
+            Head = tmp;
+
+        }
+
+        public void InsertLast(T t)
+        {
+            ListNode<T> node;
+
+            if (Head is null)
+            {
+                InsertFirst(t);
+                return;
+            }
+
+            ListNode<T> tmp = new()
+            {
+                Value = t,
+                Next = null
+            };
+
+            // Consume list nodes until reaching the last node
+            for (node = Head; node.Next is not null; node = node.Next) { }
+            node.Next = tmp;
+        }
+
+        public ListNode<T>? RemoveFirst()
+        {
+            if (Head == null) return null;
+
+            ListNode<T> tmp = Head;
+            Head = Head.Next;
+            return tmp;
+        }
+
+        public ListNode<T>? RemoveLast()
+        {
+            ListNode<T> node;
+
+            if (Head is null || Head.Next is null) return RemoveFirst();
+
+            // Traverse until pre-last node
+            for (node = Head; node.Next!.Next is not null; node = node.Next) { }
+
+            ListNode<T> tmp = node.Next;
+            node.Next = null;
+            return tmp;
+        }
+
+        public void Traverse()
+        {
+            if (Head == null)
+            {
+                Console.WriteLine("Empty List");
+                return;
+            }
+
+            for (ListNode<T>? node = Head; node is not null; node = node.Next)
+            {
+                Console.WriteLine(node.Value);
+            }
+        }
+    }
+}
---
+++ SinglyLinkedList/Program.cs
@@ -0,0 +1,24 @@
+﻿using SinglyLinkedList.Model;
+
+namespace SinglyLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SLList<int> mySSList = new();
+
+            mySSList.InsertFirst(1);
+            mySSList.InsertFirst(2);
+            mySSList.InsertFirst(3);
+            mySSList.InsertFirst(4);
+
+            //mySSList.InsertLast(5);
+            //mySSList.InsertLast(5);
+            //mySSList.InsertFirst(7);
+
+            mySSList.Traverse();
+
+        }
+    }
+}
