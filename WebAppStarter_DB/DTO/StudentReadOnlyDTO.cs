1604db4bb65661ec5a6b69b3c1f317fe6f0bfd55
Ch. Kleideris <kleiderisch@gmail.com>
2025-07-19 23:46:16 +0300

    feat(WebAppStarter_DB: add Update page

--- WebAppStarter_DB/DTO/StudentReadOnlyDTO.cs
+++ WebAppStarter_DB/DTO/StudentReadOnlyDTO.cs
@@ -2,8 +2,8 @@
 {
     public class StudentReadOnlyDTO : BaseDTO
     {
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
 
         public StudentReadOnlyDTO() { }
 
---
+++ WebAppStarter_DB/Pages/Students/Update.cshtml.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebAppStarter_DB.DTO;
+using WebAppStarter_DB.Models;
+using WebAppStarter_DB.Services;
+
+namespace WebAppStarter_DB.Pages.Students
+{
+    public class UpdateModel : PageModel
+    {
+        [BindProperty]
+        public StudentUpdateDTO StudentUpdateDTO { get; set; } = new();
+
+        public List<Error> ErrorArray { get; set; } = [];
+
+        private readonly IStudentService _studentService;
+
+        public UpdateModel(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            try
+            {
+                StudentReadOnlyDTO? studentReadOnlyDTO = _studentService.GetStudent(id);
+                StudentUpdateDTO = new StudentUpdateDTO()
+                {
+                    Id = studentReadOnlyDTO.Id,
+                    Firstname = studentReadOnlyDTO.Firstname,
+                    Lastname = studentReadOnlyDTO.Lastname,
+                };
+            }
+            catch (Exception ex)
+            {
+                ErrorArray.Add(new Error("", ex.Message, ""));
+            }
+            return Page();
+        }
+
+        public void onPost(int id)
+        {
+            try
+            {
+                StudentUpdateDTO.Id = id;
+                _studentService.UpdateStudent(StudentUpdateDTO);
+                Response.Redirect("/Students/getall");
+            }
+            catch (Exception ex)
+            {
+                ErrorArray.Add(new Error("", ex.Message, ""));
+            }
+        }
+    }
+}
